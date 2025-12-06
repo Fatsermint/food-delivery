96dfebf61a251a18f28fb83bf74446f32bc3bf87
fatsermint <kyosti.ahti@gmail.com>
2025-12-06 12:30:12 +0200

    Added Vespa and fised bugs (removed map)

--- Scenes/control.gd
+++ Scenes/control.gd
@@ -16,7 +16,6 @@ func _ready() -> void:
 
 func get_all_children(in_node, arr := []):
 	arr.push_back(in_node)
-
 	for child in in_node.get_children():
 		get_all_children(child, arr)
 
@@ -26,30 +25,29 @@ func get_all_children(in_node, arr := []):
 				print(child.name)
 				print(int(child.name))
 				add_outfit(int(child.name), $Body/Skeleton3D)
-
 			)
 	
 func add_outfit(number, new_parent):
 	var node = $Possible.get_node(str(number))
 	if new_parent.find_child(str(number)):
 		var node2 = $Body/Skeleton3D.get_node(str(number))
-		node2.get_parent().remove_child(node2) 
+		node2.get_parent().remove_child(node2)
+		if new_parent.find_child(str(number)):
+			node2.get_parent().remove_child(node2) 
+			 
 		node.global_position = Vector3(0,100,0)
 	else:
-		var new = node
-		new_parent.add_child(new)
-		new.global_position = $Body/Skeleton3D.position
-		new.rotation = $Body/Skeleton3D.rotation
+		new_parent.add_child(node)
+		node.global_position = $Body/Skeleton3D.position
+		node.rotation = $Body/Skeleton3D.rotation
 		$Phoyo.get_node(str(number)).modulate = Color(0.0, 0.0, 0.0, 0.733)
 	
 	
 #var mat = get_node("Body/Skeleton3D/Body_010").get_surface_override_materal(0)
 func _on_h_slider_slider_changed(value: float) -> void:
 	print(skinColorSlider.value)
-	#print($Body/Skeleton3D/Body_010.material_override.albedo_color)
-	#mat.albedo_color = Color(1, 0, 0)
-	#get_node("Body/Skeleton3D/Body_010").set_surface_override_material(0, mat)
 
 
 func _on_h_slider_2_value_changed(value: float) -> void:
 	print(hairColorSlider.value)
+	
---
+++ Scenes/vehicle_body_3d.gd
@@ -0,0 +1,10 @@
+extends VehicleBody3D
+
+var max_rpm = 300
+var max_torque = 150
+
+func _physics_process(delta: float) -> void:
+	steering = lerp(steering, Input.get_axis("right", "left") * 0.4, 5 * delta)
+	var acceleration = Input.get_axis("down", "forward") * 100
+	var rpm = $VehicleWheel3D.get_rpm()
+	$VehicleWheel3D.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
